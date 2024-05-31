78f320e3449a115635b72d7245f74d7a3946aeb6
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-31 17:17:01 +0300

    added category_detail view

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -5,5 +5,5 @@ from recipe.views import main, category_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
-
+    path('category/<int:category_id>/', category_detail, name='category_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe
+from .models import Recipe, Category
 import random
 
 def main(request):
@@ -7,3 +7,7 @@ def main(request):
     random_recipes = random.sample(recipes, min(len(recipes), 10))
     return render(request, 'main.html', {'recipes': random_recipes})
 
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    recipes = Recipe.objects.filter(category=category)
+    return render(request, 'category_detail.html', {'category': category, 'recipes': recipes})
