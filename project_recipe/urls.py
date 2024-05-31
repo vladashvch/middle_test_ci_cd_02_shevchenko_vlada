e1b51a6d1df73b9a138d37c87d0eb25d77713b21
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-31 17:16:04 +0300

    added main view

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from recipe.views import main, category_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name='main'),
+
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+import random
+
+def main(request):
+    recipes = list(Recipe.objects.all())
+    random_recipes = random.sample(recipes, min(len(recipes), 10))
+    return render(request, 'main.html', {'recipes': random_recipes})
 
