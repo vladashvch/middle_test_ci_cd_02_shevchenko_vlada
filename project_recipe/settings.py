d3bdaf093a2ab1e176d906d17b917358c885249d
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-05-31 17:38:23 +0300

    configured deploy to heroku using action main-deploy

--- project_recipe/settings.py
+++ project_recipe/settings.py
@@ -24,11 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+IS_DEVELOPMENT = os.environ.get("IS_DEVELOPMENT", "False") == "True"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), "*"]
 
 
 # Application definition
@@ -79,11 +86,24 @@ WSGI_APPLICATION = 'project_recipe.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "local": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "production": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
+        "TEST": {
+            "MIRROR": "default",
+        },
+    },
 }
+
+DATABASES["default"] = DATABASES["local" if IS_DEVELOPMENT else "production"]
 
 
 # Password validation
